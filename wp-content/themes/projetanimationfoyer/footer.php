ab8facad68c0427c8988994b13b767370a4f35f8
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2024-05-15 15:39:37 +0200

    150534PM1539

--- wp-content/themes/projetanimationfoyer/footer.php
+++ wp-content/themes/projetanimationfoyer/footer.php
@@ -1,6 +1,10 @@
-<footer class="crm__footer">
-		<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
-	</footer>
+
 
 <?php wp_footer(); ?>
+
+<footer class="crm__footer">
+	<div class="footer-area">
+		<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+		</div>
+</footer>
 
--- wp-content/themes/projetanimationfoyer/front-page.php
+++ wp-content/themes/projetanimationfoyer/front-page.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 	<h1>FRONT-PAGE</h1>
+	<h2>Dernières News</h2>
 <?php
 	$args = array(
         'orderby' => 'name',
@@ -29,28 +30,7 @@
             echo '<li><a href="' . get_permalink( $post->ID ) . '">' . $post->post_title . the_excerpt() . '</a></li>';
         }
         echo '</ul>';
-    } ?>
-
-	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-  
-		<article class="post">
-			<h2><?php the_title(); ?></h2>
-      
-        	<?php the_post_thumbnail(); ?>
-            
-            <p class="post__meta">
-                Publié le <?php the_time( get_option( 'date_format' ) ); ?> 
-                par <?php the_author(); ?> • <?php comments_number(); ?>
-            </p>
-            
-      		<?php the_excerpt(); ?>
-              
-      		<p>
-                <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
-            </p>
-		</article>
-
-<?php endwhile; endif; ?>
+    } ?>	
 
 <?php get_footer(); ?>
 
