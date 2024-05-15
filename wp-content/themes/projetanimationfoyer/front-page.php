1aa6a41950b31056851d92254f903c14837c56a3
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2024-05-15 14:47:06 +0200

    150524PM1447

--- wp-content/themes/projetanimationfoyer/front-page.php
+++ wp-content/themes/projetanimationfoyer/front-page.php
@@ -1,20 +1,55 @@
 <?php get_header(); ?>
 	<h1>FRONT-PAGE</h1>
+<?php
+	$args = array(
+        'orderby' => 'name',
+        'order' => 'ASC',
+        'taxonomy' => 'category'
+    );
 
-	<?php
-	$recent_posts = wp_get_recent_posts(array(
-		'numberposts' => 1, 
-		'post_status' => 'publish',
-		'post_category' => 'benevoles'
-	));
-	foreach( $recent_posts as $post_item ) : ?>
-		<li>
-			<a href="<?php echo get_permalink($post_item['ID']) ?>">
-				<?php echo get_the_post_thumbnail($post_item['ID'], 'full'); ?>
-				<p class="slider-caption-class"><?php echo $post_item['post_title'] ?></p>
-			</a>
-		</li>
-	<?php endforeach; ?>
+    $categories = get_categories( $args );
+
+    foreach( $categories as $category ) {
+        $args = array(
+            'post_type' => 'post',
+            'posts_per_page' => -1,
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'category',
+                    'field' => 'name',
+                    'terms' => $category->name
+                )
+            )
+        );
+        $posts = get_posts( $args );
+        echo '<h2>' . $category->name. '</h2>';
+        echo '<ul>';
+        foreach( $posts as $post ) {
+            echo '<li><a href="' . get_permalink( $post->ID ) . '">' . $post->post_title . '</a></li>';
+        }
+        echo '</ul>';
+    } ?>
+
+	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+  
+		<article class="post">
+			<h2><?php the_title(); ?></h2>
+      
+        	<?php the_post_thumbnail(); ?>
+            
+            <p class="post__meta">
+                Publié le <?php the_time( get_option( 'date_format' ) ); ?> 
+                par <?php the_author(); ?> • <?php comments_number(); ?>
+            </p>
+            
+      		<?php the_excerpt(); ?>
+              
+      		<p>
+                <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
+            </p>
+		</article>
+
+<?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
 
