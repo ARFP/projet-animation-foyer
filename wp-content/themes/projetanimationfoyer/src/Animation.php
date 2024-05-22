fe42b8189d75cb71b59d286610f30df5a9d5e934
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2024-05-22 11:39:22 +0200

    220524AM1139

--- wp-content/themes/projetanimationfoyer/src/Animation.php
+++ wp-content/themes/projetanimationfoyer/src/Animation.php
@@ -1,14 +1,19 @@
 <?php
 
 namespace Projet;
-use Timber\Theme as TimberTheme;
+
 use fantassin\core\WordPress\HasHooks;
+use Timber\Theme as TimberTheme;
+
 class Animation extends TimberTheme implements HasHooks{
 	
 	public function __construct() {
 		parent::__construct( slug: 'projetanimationfoyer' );
 	}
 	public function hooks() {
-	// TODO:implements hooks() methods.
+		add_filter('timber/context', [$this, 'add_to_context']);
 }
+	public function add_to_context($context) {
+		var_dump($context);
+	}
 }
